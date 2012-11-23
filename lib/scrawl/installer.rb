92fc2830eb3d17cad78c2397d517088c819bc4aa
TJ Stankus <tj@stank.us>
2012-11-23 17:03:36 -0500

    de-indent class

--- lib/scrawl/installer.rb
+++ lib/scrawl/installer.rb
@@ -1,11 +1,11 @@
 require 'fileutils'
 
 module Scrawl
-  class Installer
-    DIR_NAME = 'scrawl/entries/'
+class Installer
+  DIR_NAME = 'scrawl/entries/'
 
-    def self.install
-      FileUtils.mkdir_p(DIR_NAME) unless File.directory?(DIR_NAME)
-    end
+  def self.install
+    FileUtils.mkdir_p(DIR_NAME) unless File.directory?(DIR_NAME)
   end
+end
 end
