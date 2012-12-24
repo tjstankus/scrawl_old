7bfb03d825f879e5d23b6bf7e9ea022bb1ec0f24
TJ Stankus <tj@stank.us>
2012-12-24 14:21:35 -0500

    use a temp path to actually write to filesystem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,25 @@
 $:.unshift File.join(File.dirname(__FILE__), *%w(.. lib))
-require 'rspec'
 require 'scrawl'
+require 'rspec'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f}
 
-require 'fakefs'
+# TODO: Is this the best way to deal with a global paths?
+#       What about RSpec config?
+FIXTURES_PATH = File.join(File.dirname(__FILE__), 'fixtures')
+TMP_PATH = File.join(File.dirname(__FILE__), 'tmp')
 
 RSpec.configure do |config|
   config.include(ScrawlHelper)
+  cwd = FileUtils.pwd
+
+  config.before(:all) do
+    FileUtils.mkdir(TMP_PATH) unless File.directory?(TMP_PATH)
+    Dir.chdir(TMP_PATH)
+  end
+
+  config.after(:all) do
+    FileUtils.remove_dir(TMP_PATH, true) if File.directory?(TMP_PATH)
+    Dir.chdir(cwd)
+  end
 end
