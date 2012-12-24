cf366f27dc567f57bc2f2d0e37be9e7f4b340fac
TJ Stankus <tj@stank.us>
2012-12-24 14:23:27 -0500

    rename for improverationability

--- spec/scrawl/installer_spec.rb
+++ spec/scrawl/installer_spec.rb
@@ -1,21 +1,24 @@
 require_relative '../spec_helper'
 
 describe Scrawl::Installer do
+
   describe '.install' do
-    context 'given scrawl/entries directories do not exist' do
-      it 'creates scrawl/entries/ directories' do
+
+    context 'given scrawl/entries directory does not exist' do
+      it 'creates scrawl/entries directory' do
         expect {
           Scrawl::Installer.install
         }.to change { File.directory?('scrawl/entries/') }.from(false).to(true)
       end
     end
 
-    context 'given scrawl/entries directories exist' do
+    context 'given scrawl/entries directory exists' do
       it 'does not create directories' do
         FileUtils.mkdir_p('scrawl/entries/')
         FileUtils.should_not_receive(:mkdir_p)
         Scrawl::Installer.install
       end
     end
+
   end
 end
